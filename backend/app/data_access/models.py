bc5aa39c36fe9abc20da64ceb6701ce83a310010
Ben Cressman <ben@utdallas.edu>
2025-09-08 12:01:52 -0500

    feat: sqlalchemy model for a simple Asset

--- backend/app/data_access/models.py
+++ backend/app/data_access/models.py
@@ -1,1 +1,17 @@
 """SQLAlchemy models."""
+
+
+from sqlalchemy import Column, Integer, String, Text
+from .database import Base
+
+
+class Asset(Base):
+    __tablename__ = "assets"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True, unique=True)
+    description = Column(Text, nullable=True)
+    file_path = Column(String)
+
+    def __repr__(self):
+        return f"<Asset(id={self.id}, name=`{self.name}`)>"
