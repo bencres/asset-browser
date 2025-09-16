03dfa670b6e9eff0ebde78e0457585ff02092ee8
Ben Cressman <ben@utdallas.edu>
2025-09-16 17:45:05 -0500

    feat: more robust sqlalchemy model with single table inheritance

--- backend/app/data_access/models.py
+++ backend/app/data_access/models.py
@@ -8,10 +8,31 @@ from .database import Base
 class Asset(Base):
     __tablename__ = "assets"
 
+    # Base columns
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True, unique=True)
+    name = Column(String, index=True)
     description = Column(Text, nullable=True)
-    file_path = Column(String)
+    directory_path = Column(String)
+
+    type = Column(String, nullable=False)
+
+    # VisualAsset Columns
+    preview_image_file_path = Column(String, nullable=True)
+
+    # Enable STI
+    __mapper_args__ = {
+        "polymorphic_on": type,
+        "polymorphic_identity": "asset"
+    }
 
     def __repr__(self):
-        return f"<Asset(id={self.id}, name=`{self.name}`)>"
+        return f"<Asset(id={self.id}, name='{self.name}', type='{self.type}')>"
+
+class VisualAsset(Asset):
+    # Type for STI
+    __mapper_args__ = {
+        "polymorphic_identity": "visual_asset"
+    }
+
+    def get_preview_url(self):
+        return self.preview_image_file_path if self.preview_image_file_path else None
