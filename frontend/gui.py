26bc5283e6a702f79bc1ace868f07aed42653b30
Ben Cressman <ben@utdallas.edu>
2025-09-18 11:37:55 -0500

    feat: custom widget of icon + title for grid

--- frontend/gui.py
+++ frontend/gui.py
@@ -165,6 +165,35 @@ class FileIconWidget(QWidget):
 
 
 
+class IconGrid(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.grid = QGridLayout(self)
+        self.grid.setSpacing(2)
+        self.grid.setContentsMargins(2, 2, 2, 2)
+
+    def clear(self):
+        while self.grid.count():
+            item = self.grid.takeAt(0)
+            w = item.widget()
+            if w:
+                w.setParent(None)
+
+    def add_file_widgets(self, files):
+        """files is a list of (QPixmap, filename)"""
+        self.clear()
+        cols = 8
+        row, col = 0, 0
+        for pixmap, name in files:
+            widget = FileIconWidget(pixmap, name)
+            widget.setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
+            self.grid.addWidget(widget, row, col)
+            col += 1
+            if col >= cols:
+                col = 0
+                row += 1
+
 
 app = QApplication()
 window = MainWindow()
