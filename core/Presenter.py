675dbd9cb6c749f53687883bae82ed3d20346e3e
Ben Cressman <ben@utdallas.edu>
2025-10-04 16:45:59 -0500

    feat: skeleton transition to MPV architecture with adapters

---
+++ core/Presenter.py
@@ -0,0 +1,19 @@
+from frontend.MainWindow import MainWindow
+from backend.DataController import DataController
+
+class Presenter:
+    def __init__(self, win: MainWindow, dc: DataController):
+        self.win = win
+        self.dc = dc
+        app = self._detect_application()
+        self.adapter = self.setAdapter(app)
+
+    def _detect_application(self):
+        pass
+
+    def setAdapter(self, app):
+        pass
+
+    def loadAssets(self):
+        pass
+
