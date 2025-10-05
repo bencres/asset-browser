041f2bc0d698d102d11fa2f4a36b7583786bc82d
Ben Cressman <ben@utdallas.edu>
2025-10-04 20:36:37 -0500

    feat: skeleton Presenter implementation

--- core/Presenter.py
+++ core/Presenter.py
@@ -1,3 +1,5 @@
+from PySide6.QtWidgets import QWidget
+
 from frontend.MainWindow import MainWindow
 from backend.DataController import DataController
 
@@ -11,12 +13,41 @@ class Presenter:
     def run(self):
         self.win.show()
 
+    def set_adapter(self, app):
+        pass
+
+    def on_asset_preview_clicked(self, asset: dict):
+        pass
+
+    def on_asset_preview_double_clicked(self, asset: dict):
+        pass
+
+    def on_back_clicked(self, widget: QWidget):
+        pass
+
+    def on_import_clicked(self, asset: dict):
+        pass
+
+    def on_tree_item_clicked(self, item_idx: int):
+        pass
+
+    def on_validate_database_clicked(self, asset: dict):
+        pass
+
+    def on_edit_metadata(self, asset: dict):
+        pass
+
+    def on_save_metadata_changes(self, asset: dict):
+        pass
+
+
     def _detect_application(self):
         pass
 
-    def setAdapter(self, app):
+    def _load_assets(self):
         pass
 
-    def loadAssets(self):
+    def _build_directory_tree(self, assets: list):
         pass
+
 
