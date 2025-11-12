38dc8fe666f64e91b3b38363da25e5c7e6917b2b
Ben Cressman <ben@utdallas.edu>
2025-11-12 15:08:07 -0600

    feat: endpoint to GET a specific asset

--- src/uab/backend/app/api/routes.py
+++ src/uab/backend/app/api/routes.py
@@ -12,26 +12,45 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
+# Get endpoints
+
 
 @router.get("/", response_model=list[AssetResponse])
 def get_all_assets(db: Session = Depends(database.get_db)):
     return db.query(models.Asset).all()
 
 
+@router.get("/{asset_id}", response_model=AssetResponse)
+def get_asset(asset_id: int, db: Session = Depends(database.get_db)):
+    db_asset = db.query(models.Asset).filter(
+        models.Asset.id == asset_id).first()
+    if db_asset is None:
+        raise HTTPException(
+            status_code=404, detail=f"Asset with id `{asset_id}` not found")
+    return db_asset
+
+# Post endpoints
+
+
 @router.post("/", response_model=AssetResponse, status_code=status.HTTP_201_CREATED)
 def create_asset(asset: AssetBase, db: Session = Depends(database.get_db)):
-    db_asset = models.Asset(name=asset.name, description=asset.description, directory_path=asset.directory_path)
+    db_asset = models.Asset(
+        name=asset.name, description=asset.description, directory_path=asset.directory_path)
     db.add(db_asset)
     db.commit()
     db.refresh(db_asset)
     return db_asset
 
 
+# Put endpoints
+
 @router.put("/{asset_id}", response_model=AssetResponse)
 def update_asset(asset_id: int, asset: AssetBase, db: Session = Depends(database.get_db)):
-    db_asset = db.query(models.Asset).filter(models.Asset.id == asset_id).first()
+    db_asset = db.query(models.Asset).filter(
+        models.Asset.id == asset_id).first()
     if db_asset is None:
-        raise HTTPException(status_code=404, detail=f"Asset with id `{asset_id}` not found")
+        raise HTTPException(
+            status_code=404, detail=f"Asset with id `{asset_id}` not found")
 
     db_asset.name = asset.name
     db_asset.description = asset.description
@@ -42,22 +61,28 @@ def update_asset(asset_id: int, asset: AssetBase, db: Session = Depends(database
     return db_asset
 
 
+# Delete endpoints
+
 @router.delete("/{asset_id}", response_model=AssetResponse)
 def delete_asset(asset_id: int, db: Session = Depends(database.get_db)):
-    db_asset = db.query(models.Asset).filter(models.Asset.id == asset_id).first()
+    db_asset = db.query(models.Asset).filter(
+        models.Asset.id == asset_id).first()
     if db_asset is None:
-        raise HTTPException(status_code=404, detail=f"Asset with id `{asset_id}` not found")
+        raise HTTPException(
+            status_code=404, detail=f"Asset with id `{asset_id}` not found")
 
     db.delete(db_asset)
     db.commit()
     return db_asset
+
 
 @router.delete("/admin/clear-database",
                status_code=status.HTTP_200_OK,
                response_model=Dict[str, str])
 def clear_database(db: Session = Depends(database.get_db)):
     try:
-        table_names = [table.name for table in models.Base.metadata.sorted_tables]
+        table_names = [
+            table.name for table in models.Base.metadata.sorted_tables]
         for table_name in reversed(table_names):
             db.execute(text(f"DELETE FROM {table_name}"))
 
@@ -65,7 +90,7 @@ def clear_database(db: Session = Depends(database.get_db)):
 
         return {"message": "Database cleared successfully."}
     except Exception as e:
-        db.rollback() # Rollback if any errors
+        db.rollback()  # Rollback if any errors
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=f"Failed to clear database: {e}"
